42184d24f28894b6c3382b18cf525410edee8a02
Alex Myczko <alex@aiei.ch>
2026-01-31 20:40:12 +0100

    Update copyright attribution in modplug123.c

--- mp123/modplug123.c
+++ mp123/modplug123.c
@@ -155,14 +155,14 @@ void ansi_cursor(int visible)
 void versioninfo()
 {
 	printf("\nmodplug123 - libAO based console player - Konstanty Bialkowski");
-	printf("Based on modplugplay - Copyright (C) 2003 Gürkan Myczko\n");
+	printf("Based on modplugplay - Copyright (C) 2003 Alex Myczko\n");
 	printf("Version %s compiled on %s at %s.\n",VERSION,__DATE__,__TIME__);
 }
 
 void help(char *s, int exitcode)
 {
 	printf("modplug123 - libAO based console player - Konstanty Bialkowski\n");
-	printf("Based on modplugplay Copyright (C) 2003 Gürkan Myczko\n");
+	printf("Based on modplugplay Copyright (C) 2003 Alex Myczko\n");
         printf("Version %s compiled on %s at %s.\n",VERSION,__DATE__,__TIME__);
 	printf("\n");
 	if (exitcode!=0)
